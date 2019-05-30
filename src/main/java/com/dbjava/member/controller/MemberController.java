c1a1a8b66046d388e2f6b8ab00769358c943c41b
hyobin2 <shgyqls4@naver.com>
2019-05-30 10:03:45 +0900

    123

--- src/main/java/com/dbjava/member/controller/MemberController.java
+++ src/main/java/com/dbjava/member/controller/MemberController.java
@@ -28,7 +28,7 @@ public class MemberController {
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+		asdasdasdas
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
