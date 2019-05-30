afa4426ec68a8745d55d8974e2f614c344381838
keunsang <keun6767@gmail.com>
2019-05-30 11:55:24 +0900

    ㅋㅋ

--- src/main/java/com/zip/dbjava/member/controller/MemberController.java
+++ src/main/java/com/zip/dbjava/member/controller/MemberController.java
@@ -1,39 +1,32 @@
 package com.zip.dbjava.member.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
+//모델객체 mb를 생성하면 request영역에 
+//session이 종료 될 때까지 저장한다.
 public class MemberController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	@Autowired
+	HttpSession session;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	ModelAndView mav; //싱글턴 관리 대상 아님
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public ModelAndView main() {
+		mav=new ModelAndView();
+		mav.setViewName("main/index");//로그인 페이지로 이동
+		return mav;
 	}
-	
 }
