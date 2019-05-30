c20d519af131eb85f23d19cf6f4e1ecff933d752
EunseoPark <eeuunseoo@gmail.com>
2019-05-30 11:07:51 +0900

    property bean 만드럿셔

---
+++ src/main/java/com/dbjava/property/bean/Property.java
@@ -0,0 +1,25 @@
+package com.dbjava.property.bean;
+
+import lombok.Data;
+
+@Data
+public class Property {
+	String p_code;
+	String p_mid;
+	String p_type;
+	int p_maxguest;
+	int p_bedroom;
+	int p_bed;
+	int p_bathroom;
+	String p_location;
+	String p_convenient;
+	String p_safety;
+	String p_usespace;
+	String p_description;
+	String p_name;
+	String p_checkin;
+	String p_checkout;
+	int p_price;
+	int p_register;
+	int p_report;
+}
